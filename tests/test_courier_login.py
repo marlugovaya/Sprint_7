b2799da98c582a3608019d243de3673057edd556
Margarita Lugovaya <mmarlugovaya@yandex.ru>
2024-03-16 18:57:40 +0300

    Исправление замечания - убрала условия из тестов

--- tests/test_courier_login.py
+++ tests/test_courier_login.py
@@ -16,59 +16,38 @@ class TestCourierLogin:
             "login": login,
             "password": password
         }
-        response = requests.post(courier.courier_url+"/login", data=payload)
+        response = requests.post(courier.courier_url + "/login", data=payload)
         assert response.status_code == 200 and response.json()["id"] != " "
         courier.delete_courier(login, password)
 
     @allure.title("Проверка попытки авторизации курьера с неверным заполнением поля.")
-    @pytest.mark.parametrize(
-        "bad_field",
-        [
-            "login",
-            "password"
-        ]
-    )
-    def test_courier_login_wrong_field_false(self, bad_field):
+    def test_courier_login_wrong_field_false(self):
         courier = Courier()
         login_pass = courier.register_new_courier_and_return_login_password()
         login = login_pass[0]
         password = login_pass[1]
-        if bad_field == "login":
-            login = "this_is_a_bad_field"
-        else:
-            password = "this_is_a_bad_field"
-
-        payload = {
-            "login": login,
-            "password": password
-        }
-        response = requests.post(courier.courier_url + "/login", data=payload)
-        assert response.status_code == 404 and response.json()["message"] == "Учетная запись не найдена"
+        payloads = [
+            {"login": login,
+             "password": "this_is_bad_field"},
+            {"login": "this_is_bad_field",
+             "password": password},
+        ]
+        for p in payloads:
+            response = requests.post(courier.courier_url + "/login", data=p)
+            assert response.status_code == 404 and response.json()["message"] == "Учетная запись не найдена"
 
     @allure.title("Проверка попытки авторизации курьера без заполнения обязательного поля.")
-    @pytest.mark.parametrize(
-        "bad_field",
-        [
-            "login",
-            "password"
-        ]
-    )
-    def test_courier_login_without_field_false(self, bad_field):
+    def test_courier_login_without_field_false(self):
         courier = Courier()
         login_pass = courier.register_new_courier_and_return_login_password()
         login = login_pass[0]
         password = login_pass[1]
-        if bad_field == "login":
-            login = ""
-        else:
-            password = ""
-
-        payload = {
-            "login": login,
-            "password": password
-        }
-        response = requests.post(courier.courier_url + "/login", data=payload)
-        assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для входа"
-
-
-
+        payloads = [
+            {"login": login,
+             "password": ""},
+            {"login": "",
+             "password": password},
+        ]
+        for p in payloads:
+            response = requests.post(courier.courier_url + "/login", data=p)
+            assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для входа"
